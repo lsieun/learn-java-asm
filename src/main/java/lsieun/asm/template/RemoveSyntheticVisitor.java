0a4ebf49156f71bed69be54a6e441269cb9a3314
515882294 <515882294@qq.com>
2022-02-08 15:06:58 +0800

    feat: remove synthetic (access flag)

---
+++ src/main/java/lsieun/asm/template/RemoveSyntheticVisitor.java
@@ -0,0 +1,27 @@
+package lsieun.asm.template;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class RemoveSyntheticVisitor extends ClassVisitor {
+    public RemoveSyntheticVisitor(int api, ClassVisitor classVisitor) {
+        super(api, classVisitor);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        super.visit(version, access & (~Opcodes.ACC_SYNTHETIC), name, signature, superName, interfaces);
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+        return super.visitField(access & (~Opcodes.ACC_SYNTHETIC), name, descriptor, signature, value);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+        return super.visitMethod(access & (~Opcodes.ACC_SYNTHETIC), name, descriptor, signature, exceptions);
+    }
+}
